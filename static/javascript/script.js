7af6851eab918393fff005ccc378c511e4203a3a
LOVINE <cynthia.lovine@student.moringaschool.com>
2021-06-14 01:37:56 +0300

    added a display section

--- static/javascript/script.js
+++ static/javascript/script.js
@@ -1,25 +1,32 @@
-var akan = function(y, m, d, g) {
-    var femaleNames = ["Akosua", "Adwoa", "Abenaa", "Akua", "Yaa", "Afua", "Ama"];
-    var maleNames = ["Kwasi", "Kudwo", "Kwabena", "Kwaku", "Yaw", "Kofi", "Kwame"];
-    var d = new Date(y, --m, d);
-    if (g === "Female") {
-        return d && femaleNames[d.getDay()];
-    } else {
-        return d && maleNames[d.getDay()];
-    }
-}
+var akan = function (y, m, d, g) {
+  var femaleNames = ["Akosua", "Adwoa", "Abenaa", "Akua", "Yaa", "Afua", "Ama"];
+  var maleNames = [
+    "Kwasi",
+    "Kudwo",
+    "Kwabena",
+    "Kwaku",
+    "Yaw",
+    "Kofi",
+    "Kwame",
+  ];
+  var d = new Date(y, --m, d);
+  if (g === "Female") {
+    return d && femaleNames[d.getDay()];
+  } else {
+    return d && maleNames[d.getDay()];
+  }
+};
 
-
-$(document).ready(function() {
-    $("form#form").submit(function(event) {
-        event.preventDefault();
-        var y = parseInt($("#year").val());
-        var m = parseInt($("#month").val());
-        var d = parseInt($("#date").val());
-        var g = $("input:radio[name=gender]:checked").val();
-        var result = akan(y, m, d, g);
-        alert("Your akan name is: " + result);
-    
-        document.getElementById("form").reset();
-    });
+$(document).ready(function () {
+  $("form#form").submit(function (event) {
+    event.preventDefault();
+    var y = parseInt($("#year").val());
+    var m = parseInt($("#month").val());
+    var d = parseInt($("#date").val());
+    var g = $("input:radio[name=gender]:checked").val();
+    var result = akan(y, m, d, g);
+    document.querySelector(".Khan").style.display = "block";
+    document.getElementById("ghana").innerHTML = "Your Akan name is " + result;
+    document.getElementById("form").reset();
+  });
 });
